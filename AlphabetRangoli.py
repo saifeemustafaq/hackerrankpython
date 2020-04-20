2ce7b59e3d3a30fd8678242d0c39328d08d591c6
Mustafa Saifee <saifeemustafaq@gmail.com>
2020-04-20 19:56:28 +0530

    Create AlphabetRangoli.py

---
+++ AlphabetRangoli.py
@@ -0,0 +1,24 @@
+import string
+
+def print_rangoli(size):
+    # your code goes here
+    N = size
+    
+    alphabet = string.ascii_lowercase[:N]
+    #print(type(alphabet))
+    
+    height = N * 2 - 1
+    width = N * 4 - 3
+    lines = [None] * height
+    #print(height, width, lines, None)
+
+    for i in range(N):
+        sub_alphabet = alphabet[(-i - 1):]
+        letters = ''.join(reversed(sub_alphabet)) + sub_alphabet[1:]
+        lines[i] = lines[-i - 1] = '-'.join(letters).center(width, '-')
+    
+    print(*lines, sep='\n')
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
